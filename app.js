fd67a0dc663e0cb42340effa64437b364f0f1472
Danielle Morton <danielle@ammosquared.com>
2019-05-12 16:30:58 -0600

    all the buttons work again

--- app.js
+++ app.js
@@ -2,39 +2,38 @@
 let foes = {
   name: 'Discord',
   health: 100,
-  modifier: [],
 }
 
-function attack() {
-  document.getElementById("cooperation-button").onclick =
-    foes.health = foes.health - 1;
-
-  update()
-
+function cooperation() {
+  foes.health--;
+  update(foes.health)
 }
 
+function kindness() {
+  foes.health = foes.health - 5;
+  update(foes.health)
+}
 
+function generosity() {
+  foes.health = foes.health - 10;
+  update(foes.health)
+}
 
-// function addHealth() {
-//   foes.health = foes.health + foes.modifier
-//   update(foes.health)
-// }
+function friction() {
+  foes.health = foes.health + 2;
+  update(foes.health)
+}
 
-// function cooperation() {
-//   foes.health--;
-//   update(foes.health)
-// }
-
-// function kindness() {
-//   foes.health = foes.health - 5;
-//   update(foes.health)
-// }
-
-// function generosity() {
-//   foes.health = foes.health - 10;
-//   update(foes.health)
-// }
+function disharmony() {
+  foes.health = foes.health + 4;
+  update(foes.health)
+}
+function strife() {
+  foes.health = foes.health + 6;
+  update(foes.health)
+}
 
 function update() {
   document.getElementById("foe-health").innerText = foes.health
 }
+
